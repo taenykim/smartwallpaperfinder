d49090b848107f9d64871fd000cab36f65cae8c8
TAENY-KIM <kimtaeeeny@naver.com>
2019-12-03 20:28:04 +0900

    크롤링

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 import search_icon from "./search_icon.png";
 
@@ -6,8 +6,15 @@ class App extends Component {
   state = {
     searching_name: "",
     searching_result: "",
-    result_arr: []
+    result_arr: [],
+    page: "",
+    i: 1,
+    image_num: 0
   };
+
+  componentDidMount() {
+    window.addEventListener("scroll", this._infiniteScroll, true);
+  }
 
   handleChange = e => {
     this.setState({
@@ -17,12 +24,14 @@ class App extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    this.setState({
-      searching_result: this.state.searching_name
-    },() => {
-      this.crawling();
-    });
-      
+    this.setState(
+      {
+        searching_result: this.state.searching_name
+      },
+      () => {
+        this.crawling();
+      }
+    );
   };
 
   crawling = async () => {
@@ -31,7 +40,8 @@ class App extends Component {
     console.log(this.state.searching_result);
     const options =
       "https://cors-anywhere.herokuapp.com/https://wall.alphacoders.com/search.php?search=" +
-      this.state.searching_result;
+      this.state.searching_result +
+      this.state.page;
     const callback = await ((error, respons, body) => {
       if (error) throw error;
       const $ = cheerio.load(body);
@@ -40,14 +50,18 @@ class App extends Component {
         id,
         category,
         img;
-      const i = 0;
+
       $("#page_container > div:nth-child(6) > div.thumb-container-big").each(
         function(i, elem) {
           title = $(this)
-          .find("div.thumb-container > div.boxcaption > span.thumb-info-big").text().split("\n")[3];
+            .find("div.thumb-container > div.boxcaption > span.thumb-info-big")
+            .text()
+            .split("\n")[3];
           id = i;
           category = $(this)
-            .find("div.thumb-container > div.boxcaption > span.thumb-info-big").text().split("\n")[2];
+            .find("div.thumb-container > div.boxcaption > span.thumb-info-big")
+            .text()
+            .split("\n")[2];
           img = $(this)
             .find("div.thumb-container > div.boxgrid > a > img")
             .attr("data-src");
@@ -56,10 +70,33 @@ class App extends Component {
       );
       console.log("json: ", json);
       this.setState({
-        result_arr:this.state.result_arr.concat(json)
-      })
+        result_arr: this.state.result_arr.concat(json)
+      });
     });
     request(options, callback);
+  };
+
+  _infiniteScroll = () => {
+    let scrollHeight = Math.max(
+      document.documentElement.scrollHeight,
+      document.body.scrollHeight
+    );
+    let scrollTop = Math.max(
+      document.documentElement.scrollTop,
+      document.body.scrollTop
+    );
+    let clientHeight = document.documentElement.clientHeight;
+    if (scrollTop + clientHeight + 100 > scrollHeight) {
+      this.setState(
+        {
+          i: this.state.i + 1,
+          page: this.state.page + "&page=" + String(this.state.i)
+        },
+        () => this.crawling()
+      );
+      console.log(this.state);
+    }
+    // console.log(scrollTop, clientHeight, scrollHeight);
   };
 
   render() {
@@ -83,7 +120,12 @@ class App extends Component {
             </form>
           </div>
         </div>
-        <div className="contents">{JSON.stringify(this.state.result_arr)}</div>
+        {/* <div className="contents">{JSON.stringify(this.state.result_arr)}</div> */}
+        <div className="grid">
+          {this.state.result_arr.map(item => {
+            return <img src={item.img} className="grid-item"></img>;
+          })}
+        </div>
       </div>
     );
   }
